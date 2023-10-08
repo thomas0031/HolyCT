de8b5364191c80969cbf744cd39fc764881c3052
thomas0031 <mail@mail>
2023-10-08 12:59:38 +0300

    Updated the demo

--- main.c
+++ main.c
@@ -13,6 +13,8 @@ int main(int argc, char *argv[])
 
     context_t ctx = context_new();
     ctx->insert(ctx, "who", "World");
+    ctx->insert(ctx, "param", "lol");
+    ctx->insert(ctx, "whoo", "das");
     ctx->print(ctx);
 
     string_t rendered_page = engine->render(engine, ctx);
